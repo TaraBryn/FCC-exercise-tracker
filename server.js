f2c927a61a40722c3d06cac734fb2f844c08ff80
Glitch (tarabryn-exercise-tracker) <none>
2019-10-26 16:45:29 +0000

    📍👡 Checkpoint ./server.js:7351991/18

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const moment = require('moment');
+moment().format()
 
 const cors = require('cors')
 
