fa01b8c696a58fc21fb878d8407b1a9844e390d1
Glitch (tarabryn-exercise-tracker) <none>
2019-10-26 02:01:26 +0000

    👏🏠 Checkpoint ./server.js:7351991/35

--- server.js
+++ server.js
@@ -46,3 +46,5 @@ app.use((err, req, res, next) => {
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
+
+const Schema = mongoose.Schema;
